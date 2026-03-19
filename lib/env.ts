d52ae480200be93a35ac5ec40df17202a68a4124
AyushRawat42 <85828114+AyushRawat42@users.noreply.github.com>
2026-03-19 19:08:24 +0530

    fix(env.ts)

--- lib/env.ts
+++ lib/env.ts
@@ -12,9 +12,9 @@ const envSchema = z.object({
 })
 
 export const env = envSchema.parse({
-  APP_AWS_REGION: process.env.AWS_REGION,
-  APP_AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-  APP_AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  APP_AWS_REGION: process.env.APP_AWS_REGION,
+  APP_AWS_ACCESS_KEY_ID: process.env.APP_AWS_ACCESS_KEY_ID,
+  APP_AWS_SECRET_ACCESS_KEY: process.env.APP_AWS_SECRET_ACCESS_KEY,
   S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
   DDB_TABLE_MAIN: process.env.DDB_TABLE_MAIN,
   DDB_TABLE_QUEUE: process.env.DDB_TABLE_QUEUE,
